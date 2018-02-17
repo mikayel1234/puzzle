f592316fa1bb776bfd10e57c0f77f487d37f4582
your name <you@example.com>
2018-02-17 23:16:40 +0300

    afddsf

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<html>
+    <head>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="puzzle.js" type="text/javascript"></script>
+        <style>
+			.sd{
+				background-color:blue;
+				color:red;
+				display:inline-block;
+				width:150px;
+				height:150px;
+				
+				margin:2px;
+				z-index:1px;
+				
+			}
+			.sd1{
+				
+				display:inline-block;
+				width:150px;
+				height:150px;
+				
+				margin:2px;
+			}
+			.f
+			{
+				margin-left:10%;
+			}
+		</style>
+    </head>
+	
+    <body>
+		<img src="images/all.jpg" class="f">
+    </body>
+</html>
